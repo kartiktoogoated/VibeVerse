bb36b03ad87ade0b5e4ff5351860039ebb9d00f5
Kartik Tomar <kartiktomar112233@gmail.com>
2024-10-22 13:07:30 +0530

    Initial commit for VibeVerse project

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import styled from 'styled-components'
+
+import { useAppSelector } from './hooks'
+
+import RoomSelectionDialog from './components/RoomSelectionDialog'
+import LoginDialog from './components/LoginDialog'
+import ComputerDialog from './components/ComputerDialog'
+import WhiteboardDialog from './components/WhiteboardDialog'
+import VideoConnectionDialog from './components/VideoConnectionDialog'
+import Chat from './components/Chat'
+import HelperButtonGroup from './components/HelperButtonGroup'
+import MobileVirtualJoystick from './components/MobileVirtualJoystick'
+
+const Backdrop = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+`
 
 function App() {
-  const [count, setCount] = useState(0)
+  const loggedIn = useAppSelector((state) => state.user.loggedIn)
+  const computerDialogOpen = useAppSelector((state) => state.computer.computerDialogOpen)
+  const whiteboardDialogOpen = useAppSelector((state) => state.whiteboard.whiteboardDialogOpen)
+  const videoConnected = useAppSelector((state) => state.user.videoConnected)
+  const roomJoined = useAppSelector((state) => state.room.roomJoined)
+
+  let ui: JSX.Element
+  if (loggedIn) {
+    if (computerDialogOpen) {
+      /* Render ComputerDialog if user is using a computer. */
+      ui = <ComputerDialog />
+    } else if (whiteboardDialogOpen) {
+      /* Render WhiteboardDialog if user is using a whiteboard. */
+      ui = <WhiteboardDialog />
+    } else {
+      ui = (
+        /* Render Chat or VideoConnectionDialog if no dialogs are opened. */
+        <>
+          <Chat />
+          {/* Render VideoConnectionDialog if user is not connected to a webcam. */}
+          {!videoConnected && <VideoConnectionDialog />}
+          <MobileVirtualJoystick />
+        </>
+      )
+    }
+  } else if (roomJoined) {
+    /* Render LoginDialog if not logged in but selected a room. */
+    ui = <LoginDialog />
+  } else {
+    /* Render RoomSelectionDialog if yet selected a room. */
+    ui = <RoomSelectionDialog />
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Backdrop>
+      {ui}
+      {/* Render HelperButtonGroup if no dialogs are opened. */}
+      {!computerDialogOpen && !whiteboardDialogOpen && <HelperButtonGroup />}
+    </Backdrop>
   )
 }
 
